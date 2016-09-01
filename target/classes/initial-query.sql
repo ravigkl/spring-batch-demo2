686c384a41d39126a90910e8de4203357e5531f2
Ravi Singh <ravigkl@gmail.com>
2016-09-01 22:04:10 +0530

    remove target

--- target/classes/initial-query.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE PERSON IF EXISTS;
-
-CREATE TABLE PERSON(
-firstName VARCHAR(20),
-lastName VARCHAR(20),
-school VARCHAR(20)
-);
-
-INSERT INTO PERSON VALUES('New','User','JavaCodeGeeks');
